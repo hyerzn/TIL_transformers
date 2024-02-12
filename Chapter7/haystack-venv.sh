d50c7d73b99e43f38403303b400c70e37ceefefe
hyerzn <parkhyerin0301@gmail.com>
2024-02-12 17:36:16 +0900

    haystack - RAG

--- Chapter7/haystack-venv.sh
+++ Chapter7/haystack-venv.sh
@@ -2,10 +2,11 @@ python3 --version
 
 python3 -m venv env
 source env/bin/activate
-pip install elasticsearch-haystack==0.2.0
+pip install haystack-ai
 
-python -c "from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore"
+#python -c "from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore"
+python -c "from haystack.nodes import RAGenerator"
 
 pip install ipykernel jupyter
-python -m ipykernel install --user --name 240204_env
+python -m ipykernel install --user --name 240212_env
 deactivate
